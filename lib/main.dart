86cab84838d66188e7568c4d2d0e44c98cb04f80
Grace Njuguna <gracemakainjuguna@gmail.com>
2025-07-16 16:51:01 +0300

    Refactor main widget structure for improved layout and responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,28 @@ import '/astro_paws.dart';
 import 'overlays/game_over.dart';
 
 void main() {
-  runApp(GameWidget<AstroPawsGame>.controlled(
-    gameFactory: AstroPawsGame.new,
-    overlayBuilderMap: {
-      'MainMenu': (_, game) => MainMenu(game: game),
-      'GameOver': (_, game) => GameOver(game: game),
-      'Pause': (_, game) => PauseMenu(game: game),
-    },
-    initialActiveOverlays: const ['MainMenu'],
+  runApp(Container(
+    color: Colors.black,
+    child: LayoutBuilder(builder: (context, constraints) {
+      double maxWidth = constraints.maxWidth > 700 ? 700 : constraints.maxWidth;
+      double minWidth = constraints.maxWidth > 700 ? 700 : constraints.minWidth;
+      return Center(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidth, minWidth: minWidth),
+          child: AspectRatio(
+            aspectRatio: 9 / 16,
+            child: GameWidget<AstroPawsGame>.controlled(
+              gameFactory: AstroPawsGame.new,
+              overlayBuilderMap: {
+                'MainMenu': (_, game) => MainMenu(game: game),
+                'GameOver': (_, game) => GameOver(game: game),
+                'Pause': (_, game) => PauseMenu(game: game),
+              },
+              initialActiveOverlays: const ['MainMenu'],
+            ),
+          ),
+        ),
+      );
+    }),
   ));
 }
